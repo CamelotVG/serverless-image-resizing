dcf85aba1bb3a600a49ec5bcec2aa7c0c7b16795
George Darling <george.darling@smiledirectclub.com>
2018-08-31 12:13:38 -0500

    Changed logic to not use file extensions, using async / await, added eslint airbnb preset

--- lambda/index.js
+++ lambda/index.js
@@ -2,43 +2,36 @@
 
 // Bootstrap and config
 const AWS = require('aws-sdk');
+const Sharp = require('sharp');
+
+
 const S3 = new AWS.S3({
   signatureVersion: 'v4',
 });
-const Sharp = require('sharp');
 
-const BUCKET = process.env.BUCKET;
-const URL = process.env.URL;
+const { BUCKET, URL } = process.env;
 const ALLOWED_DIMENSIONS = new Set();
 
 if (process.env.ALLOWED_DIMENSIONS) {
   const dimensions = process.env.ALLOWED_DIMENSIONS.split(/\s*,\s*/);
-  dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension));
+  dimensions.forEach(dimension => ALLOWED_DIMENSIONS.add(dimension));
 }
 
-const formatMap = {
-  jpg: 'jpeg',
-  jpeg: 'jpeg',
-  png: 'png',
-  webp: 'webp',
+const contentTypeFormatMap = {
+  'image/jpeg': 'jpeg',
+  'image/png': 'png',
+  'image/webp': 'webp',
 };
-
-const contentTypeMap = {
-  jpg: 'image/jpeg',
-  jpeg: 'image/jpeg',
-  png: 'image/png',
-  webp: 'image/webp',
-}
 
 
 // Error response definitions
 function invalidPathResponse() {
   return {
     statusCode: '400',
-    headers: {'Content-Type': 'application/json'},
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      'code': 'INVALID_PATH',
-      'message': 'Path did not match expected format.',
+      code: 'INVALID_PATH',
+      message: 'Path did not match expected format.',
     }),
   };
 }
@@ -46,23 +39,34 @@ function invalidPathResponse() {
 function invalidDimensionsResponse() {
   const dimensions = Array.from(ALLOWED_DIMENSIONS).join(', ');
   return {
-    statusCode: '403',
-    headers: {'Content-Type': 'application/json'},
+    statusCode: '400',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      'code': 'INVALID_DIMENSIONS',
-      'message': `Allowed dimensions: ${dimensions}`,
+      code: 'INVALID_DIMENSIONS',
+      message: `Allowed dimensions: ${dimensions}`,
     }),
   };
 }
 
 function unsupportedFormatResponse() {
-  const extensions = Object.keys(formatMap).join(', ');
+  const contentTypes = Object.keys(contentTypeFormatMap).join(', ');
   return {
     statusCode: '400',
-    headers: {'Content-Type': 'application/json'},
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      'code': 'UNSUPPORTED_FORMAT',
-      'message': `Supported file extensions: ${extensions}`,
+      code: 'UNSUPPORTED_FORMAT',
+      message: `Supported content types: ${contentTypes}`,
+    }),
+  };
+}
+
+function notFoundResponse(bucket, key) {
+  return {
+    statusCode: '404',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      code: 'NOT_FOUND',
+      message: `Asset not found in bucket ${bucket} with key ${key}`,
     }),
   };
 }
@@ -71,23 +75,24 @@ function unsupportedFormatResponse() {
 
 
 // Main handler called by API gateway
-exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  // example: 'resize/75c06d3b-4342-4ab8-aa37-b1f01d654ac1/private/avatar/50x60-img123.jpg'
+exports.handler = async function handler(event, context, callback) {
+  const { key } = event.queryStringParameters;
+  // example: 'resize/75c06d3b-4342-4ab8-aa37-b1f01d654ac1/private/avatar/50x60-img123'
 
   const fullPath = key.split('/');
-  // example: ['resize', '75c06d3b-4342-4ab8-aa37-b1f01d654ac1', 'private', 'avatar', '50x60-img123.jpg']
+  // example: ['resize','75c06d3b-4342-4ab8-aa37-b1f01d654ac1','private','avatar','50x60-img123']
   if (fullPath.length < 2 || fullPath[0] !== 'resize') {
-    callback(null, invalidPathResponse()); // has to start with 'resize' and end with the filename, can have more paths in between.
+    // has to start with 'resize' and end with the asset name, can have more paths in between.
+    callback(null, invalidPathResponse());
     return;
   }
 
   const middlePath = fullPath.slice(1, fullPath.length - 1).join('/');
   // example '75c06d3b-4342-4ab8-aa37-b1f01d654ac1/private/avatar'
 
-  const dimsAndFilename = fullPath[fullPath.length - 1]; // example: '50x60-img123.jpg'
+  const dimsAndAssetName = fullPath[fullPath.length - 1]; // example: '50x60-img123'
 
-  const match = dimsAndFilename.match(/((\d+)x(\d+))-(.+)\.(.+)/);
+  const match = dimsAndAssetName.match(/((\d+)x(\d+))-(.+)/);
   if (match === null) {
     callback(null, invalidPathResponse());
     return;
@@ -95,49 +100,75 @@ exports.handler = function(event, context, callback) {
   const dimensions = match[1]; // example: '50x60'
   const width = parseInt(match[2], 10); // example: 50
   const height = parseInt(match[3], 10); // example: 60
-  const filename = match[4]; // example: 'img123'
-  const extension = match[5]; // example: 'jpg'
+  const assetName = match[4]; // example: 'img123'
 
-  const fullResKey = `assets/${middlePath}/${filename}.${extension}`;
-  // example 'assets/75c06d3b-4342-4ab8-aa37-b1f01d654ac1/private/avatar/img123.jpg'
+  const fullResKey = `assets/${middlePath}/${assetName}`;
+  // example 'assets/75c06d3b-4342-4ab8-aa37-b1f01d654ac1/private/avatar/img123'
 
   // If we are restricting the allowable dimensions, make sure the request meets that.
-  if(ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
+  if (ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
     callback(null, invalidDimensionsResponse());
     return;
   }
 
-  // check the file extension
-  const lowerExtension = extension.toLowerCase();
-  if (!formatMap.hasOwnProperty(lowerExtension)) {
+
+  // Get the full sized image out of S3
+  let data;
+  try {
+    data = await S3.getObject({ Bucket: BUCKET, Key: fullResKey }).promise();
+  } catch (e) {
+    // Check for not found error
+    if (e.code === 'NoSuchKey') {
+      callback(null, notFoundResponse(BUCKET, fullResKey));
+      return;
+    }
+    callback(e);
+    return;
+  }
+  // Check that the content type of the image is supported.
+  if (!(data.ContentType in contentTypeFormatMap)) {
     callback(null, unsupportedFormatResponse());
     return;
   }
 
-  // Get the full sized image out of S3
-  S3.getObject({Bucket: BUCKET, Key: fullResKey,}).promise()
-    // Resize it with Sharp
-    .then(data => Sharp(data.Body)
+
+  // Add a "resized-from" property to the metadata
+  const metadata = data.Metadata;
+  metadata['resized-from'] = fullResKey;
+
+
+  // Resize it with Sharp
+  let buffer;
+  try {
+    buffer = await Sharp(data.Body)
       .resize(width, height)
-      .toFormat(formatMap[lowerExtension])
-      .toBuffer()
-    )
-    // Save it for future use
-    .then(buffer => S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        ContentType: contentTypeMap[lowerExtension],
-        // TODO: Any other attributes to save?
-        Key: key, // gets saved at the path that was originally requested.
-      }).promise()
-    )
-    // Redirect to the newly created image
-    .then(() => callback(null, {
-        statusCode: '301',
-        headers: {'location': `${URL}/${key}`},
-        body: '',
-      })
-    )
-    // TODO: Catch not found error when getting the full res image.
-    .catch(err => callback(err))
-}
+      .toFormat(contentTypeFormatMap[data.ContentType])
+      .toBuffer();
+  } catch (e) {
+    callback(e);
+    return;
+  }
+
+
+  // Save the resized image to S3 for retrieving later.
+  try {
+    await S3.putObject({
+      Body: buffer,
+      Bucket: BUCKET,
+      ContentType: data.ContentType, // use the original content type
+      Metadata: metadata, // copy the original's metadata.
+      Key: key, // gets saved at the path that was originally requested.
+    }).promise();
+  } catch (e) {
+    callback(e);
+    return;
+  }
+
+
+  // Redirect to the newly created image
+  callback(null, {
+    statusCode: '301',
+    headers: { location: `${URL}/${key}` },
+    body: '',
+  });
+};
